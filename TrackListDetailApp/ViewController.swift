17b0fcb94ed84cf99a9a0d01ba3cc3e908577405
Karlo Manguiat <manguiatgkd@aub.com.ph>
2021-12-11 06:07:27 +0800

    Initial Commit

---
+++ TrackListDetailApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListDetailApp
+//
+//  Created by Application Developer 7 on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
